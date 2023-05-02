91b9c8563246a1ace9d3c6e3a477c248f5723cd2
Rajat <rajatprajapati09@gmail.com>
2023-05-02 11:56:36 +0530

    [Rajat]Address book program file

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Address Book Program ");
+
+        }
+    }
+}
